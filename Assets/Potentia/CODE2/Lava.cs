027e8ae829a114f58365c4c08d23ac8836643bd5
justsam71 <glebenkovsteplog@mail.ru>
2024-10-14 23:59:16 +0500

    123

--- Assets/Potentia/CODE2/Lava.cs
+++ Assets/Potentia/CODE2/Lava.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
-    [SerializeField] private player mage;
     private float cooldown = 1f;
     private static bool inCooldown = false;
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.tag == "Mage" && inCooldown == false)
         {
-            mage.TakeDamage(1);
+            collision.gameObject.GetComponent<player>().TakeDamage(1);
             inCooldown = true;
             Invoke("Reset", cooldown);
         }
